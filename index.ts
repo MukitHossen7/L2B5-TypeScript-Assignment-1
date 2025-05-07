10363dd2b41a8fa060136fd9324c06c1b27b2f72
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 23:10:47 +0600

    Add filterByRating function to filter items by rating

--- index.ts
+++ index.ts
@@ -8,3 +8,10 @@ function formatString(input: string, toUpper?: boolean): string {
 // console.log(formatString("Hello")); // Output: "HELLO"
 // console.log(formatString("Hello", true)); // Output: "HELLO"
 // console.log(formatString("Hello", false)); // Output: "hello"
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const filterRating = items.filter((item) => item.rating >= 4);
+  return filterRating;
+}
